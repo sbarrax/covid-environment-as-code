900eba95185923848721bc18c36532e35cc2d8ac
Marco Frattola <marco.frattola@gmail.com>
2020-04-19 22:52:48 +0200

    Create policy.js

---
+++ policy.js
@@ -0,0 +1,32 @@
+if (covidSpreadsFreelyThroughTheAir() {
+  return lockdown()
+}
+
+if (covidSpreadsOnlyFromPersonTo person through droplets then {
+  
+
+try {
+*   Wear mask and gloves when outdoor 
+*   Wash hands
+*   Hygiene first
+*   Keep 1+ meter from others
+*  Track activities/movement  with app
+
+
+} Catch(covid-19 positive) {
+
+  steadily advising if found positive
+
+} Finally {
+
+  If <it’s not enough> then {
+    Return lockdown()
+  }
+
+}
+
+}
+
+-
+
+Not enough = virus spreads at unsustainable pace
